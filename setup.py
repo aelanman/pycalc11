b97127326fe3b8cc773a1f9e73e80bf10f1b40d1
Adam Lanman <aelanman@users.noreply.github.com>
2024-12-26 22:39:48 -0500

    Add ci (#14)
    * add GH actions to run linting and unit tests
    
    * big changes to meet ruff formatting standards
    
    * blacken
    
    * fix numpy reqs for py39
    
    * rearrange into src/package directory structure
    
    * fix warnings in tests
    
    * explicitly set up fortran in GH actions
    
    ---------
    
    Co-authored-by: aelanman <adamelanman@pm.me>

--- setup.py
+++
@@ -1,7 +0,0 @@
-from skbuild import setup
-
-setup(
-    packages=['pycalc11'],
-    python_requires=">=3.7",
-    include_package_data=True,
-)
--- src/pycalc11/__init__.py
+++ src/pycalc11/__init__.py
@@ -17,6 +17,7 @@ def _format(fname):
     pth = os.path.join(DATA_PATH, fname).ljust(128)
     return pth
 
+
 # Set data file paths
 calc11.datafiles.a_tilts = _format("tilt.dat")
 calc11.datafiles.oc_file = _format("ocean_load.coef")
@@ -24,3 +25,5 @@ calc11.datafiles.optl_file = _format("ocean_pole_tide.coef")
 calc11.datafiles.dfleap = _format("ut1ls.dat")
 
 from .interface import Calc
+
+__all__ = ["Calc", "DATA_PATH", "calc11", "runner"]
